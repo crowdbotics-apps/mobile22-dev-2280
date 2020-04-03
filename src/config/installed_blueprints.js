6263154f8071dbb521b078f71d7fba8180b2bae1
Crowdbotics <team@crowdbotics.com>
2020-04-03 06:25:57 +0000

    add package blueprint-react-native-profiles-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'UserProfile1396', human_name: 'User Profile', access_route: 'UserProfile1396'},
 { name: 'EmailAuth1328', human_name: 'Email Auth', access_route: 'EmailAuth1328', icon: 'envelope-o'},
 { name: 'EmailAuth', human_name: 'EmailAuth', access_route: 'EmailAuth', icon: 'envelope-o'},
 
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import UserProfile1396Navigator from '../features/UserProfile1396/navigator';
 import EmailAuth1328Navigator from '../features/EmailAuth1328/navigator';
 import EmailAuthNavigator from '../features/EmailAuth/navigator';
 
@@ -17,6 +18,7 @@ const AppNavigator = {
       screen: SplashScreen
     },
     //@BlueprintNavigationInsertion
+UserProfile1396: { screen: UserProfile1396Navigator },
 EmailAuth1328: { screen: EmailAuth1328Navigator },
 EmailAuth: { screen: EmailAuthNavigator },
 
